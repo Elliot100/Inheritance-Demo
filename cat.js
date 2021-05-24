20821f6ffb5ef4b49ae71b3e7f6f86cb5b13ea47
Elly <ellychau10@yahoo.com>
2021-05-24 15:35:20 -0700

    Initial Commit

---
+++ cat.js
@@ -0,0 +1,9 @@
+const Cat = function(name) {
+    this.name = name;
+};
+
+Cat.prototype.meow = function() {
+    console.log(`meow, I am ${this.name}`);
+};
+
+module.exports = Cat;
---
+++ dog.js
@@ -0,0 +1,9 @@
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.woof = function() {
+    console.log(`woof, I am ${this.name}`);
+};
+
+module.exports = Dog;
